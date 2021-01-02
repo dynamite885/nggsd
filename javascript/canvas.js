727cdd96841c13a89d7b5dd252070110fce47fdf
SongJH <dynamite885@naver.com>
2021-01-02 23:58:21 +0900

    m

---
+++ javascript/canvas.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+ctx.fillRect (0, 0, 800, 800);
+ctx.fillStyle = "rgb(255, 0, 0)";
+ctx.fillRect (30, 30, 50, 50);
